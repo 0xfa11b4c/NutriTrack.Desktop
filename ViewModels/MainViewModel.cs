623749ee679c748f5d56b954d2efaf68dd099dab
Ars <0xfa11b4c@gmail.com>
2025-05-06 16:27:24 +0300

    Add properties and command to MainViewModel for inputs/calcs
    Introduce new observable properties in MainViewModel to handle
    user inputs (e.g., weight, height, age, body fat, gender, goal,
    activity level) and calculated outputs (calories, protein, fat,
    carbs). Set default values for some properties. Add a new
    CalculateCommand to trigger nutritional calculations.

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -7,6 +7,21 @@ namespace NutriTrack.Desktop.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty] private string weightInput = "";
+        [ObservableProperty] private string heightInput = "";
+        [ObservableProperty] private string ageInput = "";
+        [ObservableProperty] private string bodyFatInput = "";
+
+        [ObservableProperty] private string gender = "Male";
+        [ObservableProperty] private string goal = "Maintain";
+        [ObservableProperty] private string activityLevel = "Moderate";
+
+        [ObservableProperty] private string caloriesText = "--";
+        [ObservableProperty] private string proteinText = "--";
+        [ObservableProperty] private string fatText = "--";
+        [ObservableProperty] private string carbsText = "--";
+
+        public ICommand CalculateCommand { get; }
 
         public MainViewModel()
         {
